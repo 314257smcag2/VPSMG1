4b1cb6e090522f4698c57b9a57795085e78b121f
314257smcag2 <98874964+314257smcag2@users.noreply.github.com>
2023-01-02 19:47:25 +0100

    Update Okteto.sh

--- Okteto.sh
+++ Okteto.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 apt-get update -y && apt-get upgrade -y && apt-get install wget curl nano -y
-
 wget https://github.com/coder/code-server/releases/download/v4.9.1/code-server_4.9.1_amd64.deb
 dpkg -i code-server_4.9.1_amd64.deb
 code-server --bind-addr 127.0.0.1:12345 >> vscode.log &
 wget -O - https://deb.nodesource.com/setup_18.x | bash && apt-get -y install nodejs && npm i -g updates
-
-#wget https://deb.torproject.org/torproject.org/pool/main/t/tor/tor_0.4.7.12-1~focal+1_amd64.deb
-#dpkg -i tor_0.4.7.12-1~focal+1_amd64.deb
-
-#apt --fix-broken install -y
-apt-get install tor -y
+wget https://deb.torproject.org/torproject.org/pool/main/t/tor/tor_0.4.7.12-1~focal+1_amd64.deb
+dpkg -i tor_0.4.7.12-1~focal+1_amd64.deb
+apt --fix-broken install -y
 sed -i 's\#SocksPort 9050\SocksPort 9050\ ' /etc/tor/torrc
 sed -i 's\#ControlPort 9051\ControlPort 9051\ ' /etc/tor/torrc
 sed -i 's\#HashedControlPassword\HashedControlPassword\ ' /etc/tor/torrc
